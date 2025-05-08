96fdecc3e0f4b1a97d751cec0fbf1a2372ef5e3c
alutel201 <79305373+alutel201@users.noreply.github.com>
2025-05-08 20:44:23 +0200

    Update test_calculadora.py

--- python/test_calculadora.py
+++ python/test_calculadora.py
@@ -6,7 +6,7 @@ def multiplicar(a, b):
 class TestCalculadora(unittest.TestCase):
     def test_multiplicacion(self):
         self.assertEqual(multiplicar(2, 3), 6)
-        self.assertEqual(multiplicar(0, 10), 0)
+        self.assertEqual(multiplicar(0, 10), 1)
         self.assertEqual(multiplicar(-2, 4), -8)
 
 if __name__ == '__main__':
